5fadab23a8e0fbea967fe65d3925798364861bf4
baurine <2008.hbl@gmail.com>
2023-12-15 17:37:18 +0800

    task 1

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,8 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +26,7 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput} value={message} onChange={(e) => setMessage(e.target.value)} />
           </div>
         </div>
 
@@ -33,17 +35,19 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+            {
+              VARIANT_OPTIONS.map((variant) => (
+                <label htmlFor={`variant-${variant}`} key={variant} >
+                  <input
+                    id={`variant-${variant}`}
+                    type="radio"
+                    name="variant"
+                    value={variant}
+                  />
+                  {variant}
+                </label>
+              ))
+            }
           </div>
         </div>
 
